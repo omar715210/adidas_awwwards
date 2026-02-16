d1028665957ee7d669570dd4130c97fc682282f3
omar715210 <omar.715210@gmail.com>
2026-02-16 09:07:48 +0200

    animating the 3rd section of the pages

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -17,6 +17,7 @@ gsap.registerPlugin(ScrollTrigger)
 const Scene = ({ shirtType }: Props) => {
   return (
     <main className='min-h-screen'>
+        {/* first section */}
         <section id='first-section' className='h-screen'>
             <View className='w-dvw h-dvh'>
                 {shirtType === 'white' && <FirstWhiteModel />}
@@ -24,16 +25,18 @@ const Scene = ({ shirtType }: Props) => {
                 {shirtType === 'sport' && <FirstSportModel />}
             </View>
         </section>
+        {/* third section  */}
+        <section id='third-section' className='absolute left-0 top-[500vh] h-screen'>
+            <View className='w-dvw h-dvh'>
+               {shirtType && <ThirdModel shirtType={shirtType} />}
+            </View>
+        </section>
+        {/* second section  */}
         <section id='second-section' className='h-screen absolute inset-0 -z-10'>
             <View className='w-dvw h-dvh'>
                 {shirtType && <SecondModel shirtType={shirtType} />}
             </View>
         </section>
-        {/* <section id='third-section' className='h-screen'>
-            <View className='w-dvw h-dvh'>
-               {shirtType && <ThirdModel shirtType={shirtType} />}
-            </View>
-        </section> */}
     </main>
 )
 }
--- components/ThirdModel.tsx
+++ components/ThirdModel.tsx
@@ -1,20 +1,45 @@
 import * as THREE from 'three'
 import { ShirtType, TextureKey } from '@/lib/textures'
 import { useShirtEnvCube, useShirtSectionTextures, useShirtVideoTexture } from '@/lib/useTextures'
-import { MeshReflectorMaterial, Text } from '@react-three/drei'
-import { useCallback } from 'react'
+import { MeshReflectorMaterial, Text, useMask } from '@react-three/drei'
+import { useCallback, useRef } from 'react'
 import { shirtColors } from '@/lib/colors'
 import { ThreeEvent } from '@react-three/fiber'
 import { useMediaQuery } from 'react-responsive'
+import Masking from './Masking'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+
 
 const ThirdModel = ({shirtType}: {shirtType: ShirtType}) => {
   const isMobile = useMediaQuery({maxWidth: 1024})
+
+  const stencil = useMask(1)
+  const maskRef = useRef<THREE.Mesh>(null)
+  const groupRef = useRef<THREE.Group>(null)
+
   const textures = useShirtSectionTextures(shirtType, 'third', false) as Record<
     TextureKey<typeof shirtType, 'third'>, THREE.Texture
   >
   
   const envMap = useShirtEnvCube(shirtType)
   const video = useShirtVideoTexture(shirtType)
+
+  useGSAP(() => {
+    if (!groupRef.current || !maskRef.current) return;
+
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: "#third-section",
+        start: "top top",
+        end: "bottom top",
+        scrub: 1,
+        pin: true,
+      }
+    })
+    .from(maskRef.current.position, {y: -3})
+    .from(maskRef.current.rotation, {z: -0.3}, "<")
+  })
 
   const getTextColor = () => shirtColors[shirtType]?.text ?? 'black'
   const getWallColor = () => shirtColors[shirtType]?.wall ?? 'white'
@@ -26,68 +51,73 @@ const ThirdModel = ({shirtType}: {shirtType: ShirtType}) => {
 
   return (
     <group>
-      {/* TV */}
-      <mesh 
-        scale={isMobile ? 0.06 : 0.1} 
-        rotation-y={-Math.PI / 6} 
-        position={isMobile ? [0, 0.45, 0] : [0.2, 0.65, 0]}
-      >
-        <planeGeometry args={[16, 9]} />
-        <meshBasicMaterial  map={video}/>
-      </mesh>
-      {/* floor */}
-      <mesh position-y={0.2} rotation-x={-Math.PI / 2}>
-        <planeGeometry args={[10, 3]} />
-        <MeshReflectorMaterial 
-          envMap={envMap}
-          normalMap={textures.normal}
-          map={textures.overlay}
-          blur={[300, 30]}
-          resolution={1024}
-          mixBlur={1}
-          mixStrength={10}
-          roughness={0.8}
-          depthScale={1.2}
-          minDepthThreshold={0.4}
-          maxDepthThreshold={1.4}
-          metalness={0}
-        />
-      </mesh>
-      {/* wall */}
-      <mesh position={[0, 5, -1.3]}>
-        <planeGeometry args={[10, 10]} />
-        <meshBasicMaterial color={getWallColor()} map={textures.overlay} />
-      </mesh>
-
-      {/* HTML Content in 3D  */}
-      <group 
-        position={isMobile ? [0, 0.9, 0] : [-0.8, 0.7, 0]} 
-        rotation={isMobile ? [0, -Math.PI / 9, 0] : [0, Math.PI / 9, 0]}
-      >
-        {/* 3D logo icon */}
-        <mesh position={[0, 0.1, 0]}>
-          <planeGeometry args={[0.5, 0.2]} />
-          <meshBasicMaterial 
-            color={getTextColor()} 
-            map={textures.icon} 
-            transparent
+      <Masking ref={maskRef}/>
+      <group ref={groupRef}>
+        {/* TV */}
+        <mesh 
+          scale={isMobile ? 0.06 : 0.1} 
+          rotation-y={-Math.PI / 6} 
+          position={isMobile ? [0, 0.45, 0] : [0.2, 0.65, 0]}
+        >
+          <planeGeometry args={[16, 9]} />
+          <meshBasicMaterial  map={video} {...stencil} />
+        </mesh>
+        {/* floor */}
+        <mesh position-y={0.2} rotation-x={-Math.PI / 2}>
+          <planeGeometry args={[10, 3]} />
+          <MeshReflectorMaterial 
+            envMap={envMap}
+            normalMap={textures.normal}
+            map={textures.overlay}
+            blur={[300, 30]}
+            resolution={1024}
+            mixBlur={1}
+            mixStrength={10}
+            roughness={0.8}
+            depthScale={1.2}
+            minDepthThreshold={0.4}
+            maxDepthThreshold={1.4}
+            metalness={0}
+            {...stencil}
           />
         </mesh>
+        {/* wall */}
+        <mesh position={[0, 5, -1.3]}>
+          <planeGeometry args={[10, 10]} />
+          <meshBasicMaterial color={getWallColor()} map={textures.overlay} {...stencil} />
+        </mesh>
 
-        {/* button */}
+        {/* HTML Content in 3D  */}
         <group 
-          onClick={handleButtonClick} 
-          onPointerEnter={() => document.body.style.cursor = 'pointer'} 
-          onPointerLeave={() => document.body.style.cursor = 'default'}
+          position={isMobile ? [0, 0.9, 0] : [-0.8, 0.7, 0]} 
+          rotation={isMobile ? [0, -Math.PI / 9, 0] : [0, Math.PI / 9, 0]}
         >
-          <mesh>
-            <boxGeometry args={[0.5, 0.12, 0.02]}/>
-            <meshBasicMaterial color={getTextColor()} />
+          {/* 3D logo icon */}
+          <mesh position={[0, 0.1, 0]}>
+            <planeGeometry args={[0.5, 0.2]} />
+            <meshBasicMaterial 
+              color={getTextColor()} 
+              map={textures.icon} 
+              transparent
+              {...stencil}
+            />
           </mesh>
-          <Text fontSize={0.035} anchorX={"center"} anchorY={"middle"} position={[0, 0, 0.02]}>
-            SHOP THE COLLECTION
-            <meshBasicMaterial color={getTextColor() === "black" ? "white" : "black"}/>
-          </Text>
+
+          {/* button */}
+          <group 
+            onClick={handleButtonClick} 
+            onPointerEnter={() => document.body.style.cursor = 'pointer'} 
+            onPointerLeave={() => document.body.style.cursor = 'default'}
+          >
+            <mesh>
+              <boxGeometry args={[0.5, 0.12, 0.02]}/>
+              <meshBasicMaterial color={getTextColor()} {...stencil} />
+            </mesh>
+            <Text fontSize={0.035} anchorX={"center"} anchorY={"middle"} position={[0, 0, 0.02]}>
+              SHOP THE COLLECTION
+              <meshBasicMaterial color={getTextColor() === "black" ? "white" : "black"} {...stencil} />
+            </Text>
+          </group>
         </group>
       </group>
     </group>
